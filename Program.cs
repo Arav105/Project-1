d8ff6631b244817f83b7a48de16396b00607eb1f
Arav105 <chachuarav@gmail.com>
2023-07-24 16:52:05 +0530

    Project-1

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Path to the text file
+            string path =" G:\\Project - 1\\student details.txt";
+            string studentfile = File.ReadAllText(path);
+            Console.WriteLine(studentfile);
+            Console.ReadLine();
+        }
+    }
+    
+}
